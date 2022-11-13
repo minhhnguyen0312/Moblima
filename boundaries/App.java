35da1d5e06252b37d91efd593e106c54fc2909ad
Lim Ding Wen <limdingwen@gmail.com>
2022-11-13 20:12:31 +0800

    Add JavaDoc to App

--- boundaries/App.java
+++ boundaries/App.java
@@ -1,19 +1,32 @@
 package boundaries;
 
-
 import java.io.IOException;
 import java.text.ParseException;
 
 import entities.Person;
 
+/**
+ * Represents a generic app.
+ */
 public abstract class App {
+    /**
+     * The person currently using this app.
+     */
     public Person user;
+
     public App() {
         this.user = new Person();
     }
-    public App(Person user){
+
+    public App(Person user) {
         this.user = user;
     }
 
+    /**
+     * The main method of the app; call this to start executing.
+     * 
+     * @throws IOException
+     * @throws ParseException
+     */
     public abstract void runningSession() throws IOException, ParseException;
 }
